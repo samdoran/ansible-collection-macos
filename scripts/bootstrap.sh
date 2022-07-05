3fee09bc2312b3fae7a4953fa05e45db0306d923
Sam Doran <github@samdoran.com>
2022-07-05 12:38:25 -0400

    Add script for installing command line tools

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eu
+
+if [[ ! -d /Library/Developer/CommandLineTools ]]; then
+    echo "Installing Xcode command line tools"
+    TMP_FILE=/private/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+    touch "$TMP_FILE"
+    PACKAGE_NAME=$(sudo softwareupdate -l | grep -o 'Command Line Tools for Xcode-.*' | tail -n 1)
+    sudo softwareupdate --install "$PACKAGE_NAME"
+    rm -f "$TMP_FILE"
+fi
+
