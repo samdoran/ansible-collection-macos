ac4bca2e1cbeb1d5d753ce91ebafda21375743d2
Sam Doran <github@samdoran.com>
2022-07-07 13:02:10 -0400

    Strip newline from package name in example script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,7 +6,7 @@ if [[ ! -d /Library/Developer/CommandLineTools ]]; then
     echo "Installing Xcode command line tools"
     TMP_FILE=/private/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
     touch "$TMP_FILE"
-    PACKAGE_NAME=$(sudo softwareupdate -l | grep -o 'Command Line Tools for Xcode-.*' | tail -n 1)
+    PACKAGE_NAME=$(sudo softwareupdate -l | grep -o 'Command Line Tools for Xcode-.*' | tail -n 1 | tr -d '\n')
     sudo softwareupdate --install "$PACKAGE_NAME"
     rm -fv "$TMP_FILE"
 fi
